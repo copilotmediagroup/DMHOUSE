50d979dc8be8c302638dea39166d64f8e626d69b
copilotmediagroup <copilotmediagroup@gmail.com>
2026-07-31 09:58:14 -0400

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ixqprtabkyurqlqskjxi.supabase.co';
 
 const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4cXBydGFia3l1cnFscXNranhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA3NjAsImV4cCI6MjEwMDY0Njc2MH0.s4bYw5OGWdjZHLFDl-8_LoNngUbk7VRoxf4tH8j1Saw';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYXNlIiwicmVmIjoiaXhxcnB0YWJreXVycWxxc2tqeGkiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4NTA3MDc2MCwiZXhwIjoyMTAwNjQ2NzYwfQ.s4bYw5OGWdjZHLFDl-8_LoNngUbk7VRoxf4tH8j1Saw';
 
 export const isSupabaseConfigured = true;
 
